bd1d6dce209204ac3eed8007e0a0e5e029e277e4
Alexis Lui <alexislui@hotmail.co.uk>
2017-04-12 12:38:36 +0100

    Got access token and user information

--- src/routes/welcome.js
+++ src/routes/welcome.js
@@ -1,4 +1,6 @@
 const env2 = require('env2')('./config.env');
+const request = require('request');
+const qs = require('querystring')
 
 module.exports = {
   method: 'GET',
@@ -6,10 +8,46 @@ module.exports = {
   config: {auth: false},
   handler: (req, reply) => {
     const query = req.url.query.code;
-    console.log(query);
     const options = {
       url: `https://github.com/login/oauth/access_token?code=${query}&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`
     }
 
+    request.post(options, (err, response, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const access_token = qs.parse(body).access_token;
+        console.log("access_token", access_token);
+
+        const url = 'https://api.github.com/user';
+
+        const headers = {
+        'User-Agent': 'oauth_github_jwt',
+        Authorization: `token ${access_token}`
+        };
+
+        request.get({url:url, headers:headers}, (error, response, body) => {
+          const parsedBody = JSON.parse(body);
+
+          let options = {
+            'expiresIn': Date.now() + 24 * 60 * 60 * 1000,
+            'subject': 'github-data'
+          };
+
+          let payload = {
+            'user': {
+              'username': parsedBody.login,
+              'img_url': parsedBody.avatar_url,
+              'user_id': parsedBody.id
+            },
+            'accessToken': access_token
+          };
+        })
+
+
+      }
+    })
+    return reply("Welcome page!")
   }
+
 }
