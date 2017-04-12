9c951edd252e02ff4ec08075370eddcb40e2b8e4
Alexis Lui <alexislui@hotmail.co.uk>
2017-04-12 11:10:55 +0100

    Login now redirects to github auth Fix #1

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,18 @@
-module.exports = {
+const login = {
   method: 'GET',
   path: '/login',
   handler: (request, reply) => {
-    return reply.view('login');
+    reply.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}`);
   }
 }
+
+module.exports = login
+
+
+// module.exports = {
+//   method: 'GET',
+//   path: '/login',
+//   handler: (request, reply) => {
+//     return reply.view('login');
+//   }
+// }
